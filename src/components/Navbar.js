a6537dcbb5f82e8b014e979b91f2130ddf8d4741
Juan Pablo Gonzalez Alvarez <jugonz31@eafit.edu.co>
2019-09-23 21:05:31 -0500

    Modifying the navigation bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import {
-    Collapse,
     Navbar,
-    NavbarToggler,
     NavbarBrand,
     Nav,
     NavItem,
@@ -10,27 +8,11 @@ import {
   } from 'reactstrap';
   
   class Navigation extends Component {
-    constructor(props) {
-      super(props);
-  
-      this.toggle = this.toggle.bind(this);
-      this.state = {
-        isOpen: false
-      };
-    }
-    toggle() {
-      this.setState({
-        isOpen: !this.state.isOpen
-      });
-    }
-
     render() {
       return (
         <div>
           <Navbar className="Navbar" style={{backgroundColor: "rgba(255, 255, 255, 0)" }} dark expand="md">
             <NavbarBrand href="/" style={{color:"white"}}><i className="fa fa-2x fa-compass"></i> FastDelivery</NavbarBrand>
-            <NavbarToggler onClick={this.toggle} />
-            <Collapse isOpen={this.state.isOpen} navbar>
               <Nav className="ml-auto" navbar>
                 <NavItem>
                   <NavLink href="#"><i className="fa fa-home"></i> Home</NavLink>
@@ -48,8 +30,7 @@ import {
                   <NavLink href="#"><i className="fa fa-user"></i> Account</NavLink>
                 </NavItem>
               </Nav>
-            </Collapse>
-          </Navbar>
+          </Navbar> 
         </div>
       );
     }
